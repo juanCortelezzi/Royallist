9517c9f8d4dbe3b87a2654a9f3e92a51a5a00cbe
juancortelezzi <juanbicortelezzi@gmail.com>
2021-08-20 10:54:11 -0300

    added cli

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module royallist
 
 go 1.14
 
-require github.com/gookit/color v1.3.8
+require (
+	github.com/gookit/color v1.3.8
+	github.com/urfave/cli/v2 v2.3.0 // indirect
+)
--- royallist.go
+++ royallist.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"github.com/urfave/cli/v2"
 	"io/ioutil"
 	"log"
 	"os"
@@ -10,30 +11,40 @@ import (
 )
 
 func main() {
-	path := "./"
-	if len(os.Args[1:]) > 0 {
-		path = os.Args[1]
+	app := &cli.App{
+		Name:  "royallist",
+		Usage: "Colorfull and simple ls",
+		Flags: []cli.Flag{
+			&cli.BoolFlag{
+				Name:    "recursive",
+				Usage:   "recursive view of the directory",
+				Aliases: []string{"r"},
+			},
+		},
+		Action: royallist,
 	}
 
-	files, err := ioutil.ReadDir(path)
+	err := app.Run(os.Args)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	directoryArr := make([]string, 0, len(files))
-	fileArr := make([]string, 0, len(files))
+}
 
-	for _, f := range files {
-		fmt.Println("raw: ", f.Name())
+func printContentsFromPath(path string) {
+	contents, err := ioutil.ReadDir(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var fileArr []string
+
+	for _, f := range contents {
 		if f.IsDir() {
-			directoryArr = append(directoryArr, f.Name())
+			fmt.Printf("%s %s\n", icons.Names["directory"], f.Name())
 		} else {
 			fileArr = append(fileArr, f.Name())
 		}
-	}
-
-	for _, directory := range directoryArr {
-		fmt.Printf("%s %s\n", icons.Names["directory"], directory)
 	}
 
 	for _, file := range fileArr {
@@ -51,4 +62,28 @@ func main() {
 
 		fmt.Printf("%s %s\n", icons.Names["file"], file)
 	}
+}
+
+func royallist(c *cli.Context) error {
+	if c.Bool("recursive") {
+		fmt.Println("recursive not supported yet")
+		return nil
+	}
+
+	if c.Args().Len() == 1 {
+		printContentsFromPath(c.Args().First())
+		return nil
+	}
+
+	if c.Args().Len() > 1 {
+		for _, path := range c.Args().Slice() {
+			fmt.Printf("\n%s\n------------\n\n", path)
+			printContentsFromPath(path)
+		}
+		return nil
+	}
+
+	printContentsFromPath("./")
+
+	return nil
 }
